9539d8a15919b169e3e1a3eeb5dbd36e43662f38
Michael Matloka <dev@twixes.com>
2021-07-11 15:36:08 +0200

    Add scaffolding, eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: "next",
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'promise', 'simple-import-sort'],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:promise/recommended', 'prettier'],
+    ignorePatterns: ['bin', 'node_modules'],
+    rules: {
+        'space-before-function-paren': [
+            'error',
+            {
+                named: 'never'
+            }
+        ],
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'off'
+    }
+}
