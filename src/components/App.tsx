706431c36b11cef3c7a73d8c0f5c4634107fef5f
Michael Matloka <dev@twixes.com>
2021-07-11 18:47:19 +0200

    Improve <main> styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,19 @@
 /** @jsxImportSource @emotion/react */
 
+import { css } from '@emotion/react'
 import { ErrorBoundary } from '@sentry/react'
 import { FallbackRender } from '@sentry/react/dist/errorboundary'
 import posthog from 'posthog-js'
 import { Route, useHistory } from 'react-router-dom'
+
+const main = css({
+    margin: '1rem auto',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center'
+})
 
 export default function App(): JSX.Element {
     const history = useHistory()
@@ -13,7 +23,7 @@ export default function App(): JSX.Element {
     })
 
     return (
-        <main style={{ margin: '0 auto', textAlign: 'center'}}>
+        <main css={main}>
             <h1>You are</h1>
             <img src="/point.png" style={{maxWidth: '12rem'}} />
             <h1>right there</h1>
